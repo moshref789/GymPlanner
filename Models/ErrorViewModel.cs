e6d3fb13d15c4771e1a0ca9a7476ff193fff43d1
moshref789 <moshref789@gmail.com>
2025-12-02 17:26:12 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GymPlanner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
