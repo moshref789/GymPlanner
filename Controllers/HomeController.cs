2f86c57b5b913bf8df70a756b4f08022df9f243a
omar-ys <omaryousef2y@gmail.com>
2025-12-04 03:36:05 +0300

    Add cookie  and user account
    Enhanced `HomeController` to set cookies for user authentication
    status and browser information. Introduced a `SetCookies` method
    to manage cookie creation with security options.
    
    Updated `_Layout.cshtml` to include a navigation link to the new
    "User Account" page. Added `AccountController` with an `Index`
    action and created an `Index.cshtml` view to display account
    information and list cookies.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,7 +8,24 @@ namespace GymPlanner.Controllers
     {
         public IActionResult Index()
         {
+
+            // Check if the user is authenticated
+            if (User.Identity.IsAuthenticated)
+            {
+                // User is logged in, get their username
+                SetCookies("userName"
+               , User.Identity.Name);
+            }
+            else
+            {
+                // User is not logged in, set a cookie with "Guest"
+                SetCookies("userName"
+               , "guest");
+            }
+            SetCookies("broswerName"
+           , Request.Headers["User-Agent"].ToString());
             return View();
+
         }
         public IActionResult Privacy()
         {
@@ -24,6 +41,19 @@ namespace GymPlanner.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult SetCookies(string cookieName, string cookieValue)
+        {
+            CookieOptions options = new CookieOptions
+            {
+                Expires = DateTime.Now.AddDays(15), // Cookie expires in 14 days
+                HttpOnly = true, // Prevent JavaScript access to the cookies
+                Secure = true, // Use Secure flage
+                SameSite = SameSiteMode.Strict // Prevent CSRF attacks
+            };
+            Response.Cookies.Append(cookieName, cookieValue, options);
+            return Ok("Cookies has been set.");
         }
     }
 }
